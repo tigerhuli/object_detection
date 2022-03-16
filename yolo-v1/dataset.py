928d52c825b90af093181920c5ead5b344821fbd
tigerhuli <tigerhuli@tencent.com>
2022-03-16 14:30:26 +0800

    add

--- yolo-v1/dataset.py
+++ yolo-v1/dataset.py
@@ -1,11 +1,14 @@
-from torch.utils.data import Dataset, DataLoader
 import os
-import sys
+from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from torchvision import transforms
+import cv2
+import pandas as pd
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
 
 
 class TrainDataset(Dataset):
@@ -43,13 +46,46 @@ class TrainDataset(Dataset):
         return image_tensor, target
 
 
+def targets2objects(targets, image_width, image_height, label_name_map):
+    grid_size = int(image_width)/7
+    objs = []
+    for i in range(targets.shape[0]):
+        for j in range(targets.shape[1]):
+            if targets[i][j][4] == 0:
+                continue
+            box = targets[i][j][0:4]
+            obj_x, obj_y, obj_width, obj_height = (i+box[0])*grid_size, (j+box[1])*grid_size, box[2]*image_width, box[3]*image_height
+            obj_xmin, obj_ymin, obj_xmax, obj_ymax = int(obj_x-obj_width/2), int(obj_y-obj_height/2), int(obj_x+obj_width/2), int(obj_y+obj_height/2)
+            one_hot = targets[i][j][10:]
+            obj_name = label_name_map[np.argmax(one_hot)]
+            obj = (obj_xmin, obj_ymin, obj_xmax, obj_ymax, obj_name)
+            objs.append(obj)
+    return obj_name, objs
+
+
 if __name__ == '__main__':
+    from tools import detection_box
     dataset = TrainDataset()
-    dataloader = DataLoader(dataset, batch_size=64)
-    x, y = next(iter(dataloader))
-    image = x[0]
-    image = image.squeeze().permute(1, 2, 0)
-    print(image.dtype, y.dtype)
-    plt.figure()
-    plt.imshow(image)
-    plt.show()
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+    count = 0
+    for x, y in dataloader:
+        image = x[0]
+        image = image.squeeze().permute(1, 2, 0).numpy()
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        label_name_map_data = pd.read_csv('data/class_label_map.csv')
+        label_name_map = {}
+        for name, label in zip(label_name_map_data['classes name'], label_name_map_data['label']):
+            label_name_map[int(label)] = name
+        image_width, image_height = image.shape[0], image.shape[1]
+        targets = y[0].numpy()
+        obj_name, objs = targets2objects(targets, image_width, image_height, label_name_map)
+        if len(objs) == 1:
+            continue
+        image = detection_box.add_detection_boxes(image, objs)
+
+        cv2.imshow(f'{obj_name}_{count}', image)
+        count += 1
+        if count > 10:
+            break
+    cv2.waitKey(0)
--- yolo-v1/label_transform.py
+++ yolo-v1/label_transform.py
@@ -20,13 +20,14 @@ def extract_label_from_xml():
         info = {}
         info['size'] = (width, height)
         objs = []
-        for xml_obj in data.xpath('//annotation/object'):
-            name = xml_obj.xpath('//object/name/text()')[0]
-            xmin = xml_obj.xpath('//object/bndbox/xmin/text()')[0]
-            ymin = xml_obj.xpath('//object/bndbox/ymin/text()')[0]
-            xmax = xml_obj.xpath('//object/bndbox/xmax/text()')[0]
-            ymax = xml_obj.xpath('//object/bndbox/ymax/text()')[0]
-            objs.append((name, xmin, ymin, xmax, ymax))
+        name_list = data.xpath('/annotation/object/name/text()')
+        xmin_list = data.xpath('/annotation/object/bndbox/xmin/text()')
+        ymin_list = data.xpath('/annotation/object/bndbox/ymin/text()')
+        xmax_list = data.xpath('/annotation/object/bndbox/xmax/text()')
+        ymax_list = data.xpath('/annotation/object/bndbox/ymax/text()')
+        for name, xmin, ymin, xmax, ymax in zip(name_list, xmin_list, ymin_list, xmax_list, ymax_list):
+            obj = (name, xmin, ymin, xmax, ymax)
+            objs.append(obj)
             obj_count += 1
             classes_set.add(name)
 
@@ -34,7 +35,6 @@ def extract_label_from_xml():
         image = data.xpath('//annotation/filename/text()')[0]
         image2info[image] = info
 
-    print(f'obtain images: {len(image2info)}, objects: {obj_count}, classes: {len(classes_set)}')
     return image2info, classes_set
 
 
@@ -62,18 +62,20 @@ def generate_targets(image2info, classname2label):
         image_width = int(info['size'][0])
         image_height = int(info['size'][1])
 
-        grid_width = int(image_width/S)
-        grid_height = int(image_height/S)
+        grid_width = image_width/S
+        grid_height = image_height/S
 
         targets = np.zeros((S, S, 30))
-        for object in info['objects']:
-            xmin = int(float(object[1]))
-            ymin = int(float(object[2]))
-            xmax = int(float(object[3]))
-            ymax = int(float(object[4]))
+        objects = info['objects']
 
-            x = int((xmin+xmax)/2)
-            y = int((ymin+ymax)/2)
+        for object in objects:
+            xmin = float(object[1])
+            ymin = float(object[2])
+            xmax = float(object[3])
+            ymax = float(object[4])
+
+            x = (xmin+xmax)/2
+            y = (ymin+ymax)/2
             w = xmax-xmin
             h = ymax-ymin
 
